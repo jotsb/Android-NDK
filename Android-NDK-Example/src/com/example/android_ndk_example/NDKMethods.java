d773689d0712cc5ab4619af34db15e05c7766d27
jotsb <ekomkar@hotmail.com>
2015-09-30 21:36:36 -0700

    removing this code for as there will not be any direct access to JNI code

--- Android-NDK-Example/src/com/example/android_ndk_example/NDKMethods.java
+++ Android-NDK-Example/src/com/example/android_ndk_example/NDKMethods.java
@@ -1,9 +1,9 @@
 package com.example.android_ndk_example;
 
 public class NDKMethods {
-	public native static String set_msg(String text);
+	/*public native static String set_msg(String text);
 
 	static {
 		System.loadLibrary("com_example_android_ndk_example_NDKMethods");
-	}
+	}*/
 }
