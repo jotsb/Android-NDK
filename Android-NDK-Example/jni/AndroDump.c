5714ed64e3c637d64ac07f5d6e77d2097d01e839
jotsb <ekomkar@hotmail.com>
2015-10-10 20:58:05 -0700

    Working with building Executable

---
+++ Android-NDK-Example/jni/AndroDump.c
@@ -0,0 +1,64 @@
+/* Header for class com_example_android_ndk_example_NDKMethods*/
+#include "AndroDump.h"
+
+#define DEBUG_TAG "\nLIBPCAP_DEBUGGING =============> "
+
+int main() {
+
+	char *dev; /* name of the device to use */
+	char *net; /* dot notation of the network address */
+	char *mask;/* dot notation of the network mask    */
+	char errbuf[PCAP_ERRBUF_SIZE];
+
+	int ret; /* return code */
+
+	bpf_u_int32 netp; /* ip          */
+	bpf_u_int32 maskp;/* subnet mask */
+
+	struct in_addr addr;
+
+	//const char *str = (*env)->GetStringUTFChars(env, javaString, NULL);
+
+	setuid(0);
+	setgid(0);
+
+	dev = pcap_lookupdev(errbuf);
+
+	if (dev == NULL) {
+		submit_log("errbuf: [%s]", errbuf);
+		exit(1);
+	}
+	submit_log("Device: [%s]", dev);
+
+	ret = pcap_lookupnet(dev, &netp, &maskp, errbuf);
+
+	if (ret == -1) {
+		submit_log("errbuf: [%s]", errbuf);
+		exit(1);
+	}
+
+	/* get the network address in a human readable form */
+	addr.s_addr = netp;
+	net = inet_ntoa(addr);
+	if (net == NULL) {
+		submit_log("errbuf: [%s]", "inet_ntoa failed");
+		exit(1);
+	}
+	submit_log("Net status: [%s]", net);
+
+	addr.s_addr = maskp;
+	mask = inet_ntoa(addr);
+	if (mask == NULL) {
+		submit_log("errbuf: [%s]", "inet_ntoa failed");
+		exit(1);
+	}
+
+	submit_log("MASK: [%s]", mask);
+
+	return 0;
+}
+
+int submit_log(char *msgType, char *string) {
+	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, msgType, string);
+	return 0;
+}
