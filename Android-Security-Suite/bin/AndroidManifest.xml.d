632f8b57df18a09730fc20385a3cc96b7dde6cdf
jotsb <ekomkar@hotmail.com>
2015-11-16 21:16:45 -0800

    Removed the .class files from git

--- Android-Security-Suite/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/jb/Documents/Android/Workspace/Android-NDK/Android-Security-Suite/bin/AndroidManifest.xml : \
-/home/jb/Documents/Android/Workspace/Android-NDK/Android-Security-Suite/AndroidManifest.xml \
