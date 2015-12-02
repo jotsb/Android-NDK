1b8df5fa04f8ff13491f3552f5c2c0defbf73ee4
jotsb <ekomkar@hotmail.com>
2015-12-01 18:01:39 -0800

    pkt_callback is now executing with pcap_loop

--- Android-Security-Suite/jni/AndroDump.c
+++ Android-Security-Suite/jni/AndroDump.c
@@ -1,71 +1,195 @@
 /* Header for class com_example_android_ndk_example_NDKMethods*/
 #include "AndroDump.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h>
+#include <time.h> 
+
+
 #define DEBUG_TAG "\n[ANDROID_SECURITY_SUITE] ===> LIBPCAP_DEBUGGING ======> "
+
+// Check all the headers in the Ethernet frame
+void pkt_callback(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet) {
+	static int count = 1;
+	submit_log("pkt_callback(): [%s]\n", "Running this function");
+	fprintf(stdout,"%d.. ",count);
+	fflush(stdout);
+	count++;
+	
+}
 
 int main(int argc, char **argv) {
 
-	// Device name, network address, network mask
-	char *dev, *net, *mask;
+	char *dev; // Network Device
 	char errbuf[PCAP_ERRBUF_SIZE];
-	int ret; // return code
-	bpf_u_int32 netp, maskp; // ip, subnet mask
-	struct in_addr addr;
-	pcap_t* nic_descr;
-	u_char* args = NULL;
-
-	//const char *str = (*env)->GetStringUTFChars(env, javaString, NULL);
+	pcap_t *nic_descr;
+    const u_char *packet;
+    struct pcap_pkthdr pkt_hdr;     // defined in pcap.h
+    int loop_ret; 
 
 	// find the first NIC that is up and sniff packets from it
 	dev = pcap_lookupdev(errbuf);
 	if (dev == NULL) {
-		submit_log("errbuf: [%s]\n", errbuf);
+		submit_log("pcap_lookupdev => errbuf: [%s]\n", errbuf);
 		exit(1);
 	}
 	submit_log("Device: [%s]\n", dev);
 
-	// Use pcap to get the IP address and subnet mask of the device
-	ret = pcap_lookupnet(dev, &netp, &maskp, errbuf);
-	if (ret == -1) {
-		submit_log("errbuf: [%s]\n", errbuf);
-		exit(1);
-	}
-
 	// open device for reading
-	nic_descr = pcap_open_live(dev, BUFSIZ, 1, -1, errbuf);
-	if (nic_descr = NULL) {
-		submit_log("pcap_open_live(): %s \n", errbuf);
+	nic_descr = pcap_open_live(dev, BUFSIZ, 1, 0, errbuf);
+	if (nic_descr == NULL) {
+		submit_log("pcap_open_live() => errbuf: [%s] \n", errbuf);
 		exit(1);
 	}
+	submit_log("pcap_open_live(): [%s]\n", "Running this function");
 
-	pcap_loop(nic_descr, atoi(argv[1]), pkt_callback, args);
+	loop_ret = pcap_loop(nic_descr, -1, pkt_callback, NULL);
 
-	/* get the network address in a human readable form
-	 addr.s_addr = netp;
-	 net = inet_ntoa(addr);
-	 if (net == NULL) {
-	 submit_log("errbuf: [%s]\n", "inet_ntoa failed");
-	 exit(1);
-	 }
-	 submit_log("Net status: [%s]\n", net);
+	submit_log_i("pcap_loop(): loop_ret = [%d]\n", loop_ret);
 
-	 addr.s_addr = maskp;
-	 mask = inet_ntoa(addr);
-	 if (mask == NULL) {
-	 submit_log("errbuf: [%s]\n", "inet_ntoa failed");
-	 exit(1);
-	 }
-	 submit_log("MASK: [%s]\n", mask);*/
+	// capture a single packet
+	//packet = pcap_next(nic_descr, &pkt_hdr);
+	//if(packet == NULL) {
+	//	submit_log("pcap_next() => errbuf: [%s] \n", "Packet capture failed");
+	//	exit(1);
+	//}
+
+	// Close the connection
+	pcap_close(nic_descr);
 
 	return 0;
 }
 
-void pkt_callback(u_char *ptr_null, const struct pcap_pkthdr* pkthdr,
-		const u_char* packet) {
+
+
+/*u_int16_t handle_ethernet (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet)
+{
+    	u_int caplen = pkthdr->caplen;
+    	u_int length = pkthdr->len;
+    	struct ether_header *eptr;  //net/ethernet.h 
+    	u_short ether_type;
+
+    	if (caplen < ETHER_HDRLEN)
+    	{
+        	submit_log("Packet length less than ethernet header length\n", "");
+        	return -1;
+    	}
+
+    	// Start with the Ethernet header... 
+    	eptr = (struct ether_header *) packet;
+    	ether_type = ntohs(eptr->ether_type);
+
+    	// Print SOURCE DEST TYPE LENGTH fields
+   		fprintf(stdout,"ETH: ");
+    	fprintf(stdout,"%s ", ether_ntoa((struct ether_addr*)eptr->ether_shost));
+    	fprintf(stdout,"%s ",ether_ntoa((struct ether_addr*)eptr->ether_dhost));
+
+    	// Check to see if we have an IP packet 
+    	if (ether_type == ETHERTYPE_IP)
+    	{
+        	fprintf(stdout,"(IP)");
+    	}
+		else  if (ether_type == ETHERTYPE_ARP)
+    	{
+        	fprintf(stdout,"(ARP)");
+    	}
+		else  if (eptr->ether_type == ETHERTYPE_REVARP)
+    	{
+        	fprintf(stdout,"(RARP)");
+    	}
+		else 
+		{
+        	fprintf(stdout,"(?)");
+    	}
+    	fprintf(stdout," %d\n",length);
+
+    	return ether_type;
 }
+
+// This function will parse the IP header and print out selected fields of interest
+u_char* handle_IP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet)
+{
+    	const struct my_ip* ip;
+    	u_int length = pkthdr->len;
+    	u_int hlen,off,version;
+    	int len;
+
+    	// Jump past the Ethernet header 
+    	ip = (struct my_ip*)(packet + sizeof(struct ether_header));
+    	length -= sizeof(struct ether_header); 
+
+    	// make sure that the packet is of a valid length 
+    	if (length < sizeof(struct my_ip))
+    	{
+        	fprintf(stderr,"Truncated IP %d",length);
+        	return NULL;
+    	}
+
+    	len     = ntohs(ip->ip_len);
+    	hlen    = IP_HL(ip); 	// get header length 
+    	version = IP_V(ip);	// get the IP version number
+
+    	// verify version 
+    	if(version != 4)
+    	{
+      		fprintf(stderr,"Unknown version %d\n",version);
+      		return NULL;
+    	}
+
+    	// verify the header length 
+    	if(hlen < 5 )
+    	{
+        	fprintf(stderr,"Bad header length %d \n", hlen);
+    	}
+
+    	// Ensure that we have as much of the packet as we should 
+    	if (length < len)
+        	printf("\nTruncated IP - %d bytes missing\n",len - length);
+
+    	// Ensure that the first fragment is present
+    	off = ntohs(ip->ip_off);
+    	if ((off & 0x1fff) == 0 ) 	// i.e, no 1's in first 13 bits 
+    	{				// print SOURCE DESTINATION hlen version len offset 
+        	fprintf(stdout,"IP: ");
+        	fprintf(stdout,"%s ", inet_ntoa(ip->ip_src));
+        	fprintf(stdout,"%s %d %d %d %d\n", inet_ntoa(ip->ip_dst), hlen,version,len,off);
+    	}
+
+    	switch(ip->ip_p) 
+		{
+			case IPPROTO_TCP:
+				handle_TCP(args, pkthdr, packet);
+				break;
+		}
+
+    	return NULL;
+}
+
+u_char* handle_TCP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet)
+{
+	const struct sniff_tcp *tcp;
+	const struct my_ip* ip;
+	const char *payload, *tcp_payload;
+	unsigned short sport, dport;
+	int size_ip, size_tcp;
+	
+	return NULL;
+}*/
 
 int submit_log(char *msgType, char *string) {
 	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, msgType, string);
+	//printf(msgType, string);
+	return 0;
+}
+
+int submit_log_i(char *msgType, int value) {
+	__android_log_print(ANDROID_LOG_DEBUG, DEBUG_TAG, msgType, value);
 	//printf(msgType, string);
 	return 0;
 }
--- Android-Security-Suite/jni/AndroDump.h
+++ Android-Security-Suite/jni/AndroDump.h
@@ -1,9 +1,12 @@
 /* Header for class com_example_android_ndk_example_NDKMethods */
 #undef __cplusplus
 #undef _GNU_SOURCE
+
+// NDK Header Files
 #include <jni.h>
 #include <android/log.h>
-
+/*
+// Regular C Header Files
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
@@ -13,25 +16,22 @@
 #include <time.h>
 #include <signal.h>
 #include <ctype.h>
-
 #include <pthread.h>
 
-/*
+// Network Header Files
+#include <netinet/ip.h>
+#include <netinet/if_ether.h> 
+#include <net/ethernet.h>
+#include <netinet/ether.h> 
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <pcap.h>
 #include <netinet/tcp.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 */
 
-#include <netinet/ip.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
 
-
-#include <pcap.h>
-
-#ifndef NULL
-#define NULL   ((void *) 0)
-#endif
 
 
 #ifndef _Included_AndroDump
@@ -39,17 +39,73 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-/*
- * Class:     com_example_hello_c_world_NDKMethods
- * Method:    set_msg
- * Signature: (Ljava/lang/String;)Ljava/lang/String;
- */
+
 //JNIEXPORT jstring JNICALL ava_com_example_android_1ndk_1example_NDKMethods_set_1msg
 //  (JNIEnv *, jclass, jstring);
 
+
+// tcpdump header (ether.h) defines ETHER_HDRLEN) 
+#ifndef ETHER_HDRLEN 
+#define ETHER_HDRLEN 14
+#endif
+
 int submit_log(char *msgType, char *string);
 
-void pkt_callback(u_char *ptr_null, const struct pcap_pkthdr* pkthdr, const u_char* packet);
+/*void pkt_callback(u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet);
+u_int16_t handle_ethernet (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet);
+u_char* handle_IP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet);
+u_char* handle_TCP (u_char *args,const struct pcap_pkthdr* pkthdr,const u_char* packet);*/
+
+
+/*
+ * Structure of an internet header, stripped of all options.
+ *
+ * This is taken directly from the tcpdump source
+ *
+ * We declare ip_len and ip_off to be short, rather than u_short
+ * pragmatically since otherwise unsigned comparisons can result
+ * against negative integers quite easily, and fail in subtle ways.
+ */
+/*struct my_ip {
+	u_int8_t	ip_vhl;		// header length, version 
+#define IP_V(ip)	(((ip)->ip_vhl & 0xf0) >> 4)
+#define IP_HL(ip)	((ip)->ip_vhl & 0x0f)
+	u_int8_t	ip_tos;		// type of service 
+	u_int16_t	ip_len;		// total length 
+	u_int16_t	ip_id;		// identification 
+	u_int16_t	ip_off;		// fragment offset field 
+#define	IP_DF 0x4000			// dont fragment flag 
+#define	IP_MF 0x2000			// more fragments flag
+#define	IP_OFFMASK 0x1fff		// mask for fragmenting bits 
+	u_int8_t	ip_ttl;		// time to live 
+	u_int8_t	ip_p;		// protocol 
+	u_int16_t	ip_sum;		// checksum 
+	struct	in_addr ip_src,ip_dst;	// source and dest address *
+};
+
+// TCP header
+
+struct sniff_tcp {
+        u_short th_sport;               // source port 
+        u_short th_dport;               // destination port 
+        u_int th_seq;                 // sequence number 
+        u_int th_ack;                 // acknowledgement number 
+        u_char  th_offx2;               // data offset, rsvd 
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 // window 
+        u_short th_sum;                 // checksum 
+        u_short th_urp;                 // urgent pointer 
+};*/
 
 #ifdef __cplusplus
 }
